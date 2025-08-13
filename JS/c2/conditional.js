3804289d763360d6faa218bd8d6ed7502a02c845
DakshKhandelwal <56481845+DakshKhandelwal@users.noreply.github.com>
2025-08-13 23:32:30 +0530

    Updated code

--- JS/c2/conditional.js
+++ JS/c2/conditional.js
@@ -55,13 +55,31 @@
 
 // 3) else-if statements
 
-let age = 17;
-if(age< 18) {
-    console.log("child");
-}
-else if(age > 60) {
-    console.log("senior");
-}
-else {
-    console.log("adult");
-}
+// let age = 17;
+// if(age< 18) {
+//     console.log("child");
+// }
+// else if(age > 60) {
+//     console.log("senior");
+// }
+// else {
+//     console.log("adult");
+// }
+
+// Ternary Operators 
+// condition ? true output : false output
+
+let age = 21;
+age > 18 ? console.log("adult") : console.log("not adult");
+
+// Question: Get a number input from the user. And check if the number is multiple of 7 or not?
+
+// let number = prompt("Enter a number: ");
+// // console.log(number);
+
+// if(number % 7 === 0) {
+//     console.log( number , " is multiple of 7");
+// }
+// else {
+//     console.log( number , " is not multiple of 7");
+// }
